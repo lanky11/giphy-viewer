10a9a61d2a67ede1f271a4171418bdcfb04d7cf9
Leif Swenson <leifswenson@gmail.com>
2017-12-22 16:56:35 +0000

    more work on js to display gifs and buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,19 +2,28 @@
 // Array to hold buttons
 var gifArray = ["frog", "fish", "dog", "cat"];
 
-for(var j=0; j<gifArray.length; j++) {
+
+// Adding and updating gif buttons
+//==================================================================================================
+
+// Creates a gif button
+function createButton() {
+  
+  // remove gifs from page
+  $(".gif-display").empty();
+  $(".gif-button-display").empty();
+  
+  // Loop to create buttons
+  for(var j=0; j<gifArray.length; j++) {
     console.log(gifArray[j]);
     
-    // create button, add a class, an attrabute and append to div
+    // create button, add text, a class, an attrabute and append to div
     var gifButton = $("<button>").text(gifArray[j]);
     gifButton.addClass("btn btn-success m-1");
     gifButton.attr("data-button", gifArray[j]);
     $(".gif-button-display").append(gifButton);
+  }
 }
-
-
-// Add Gif Buttons
-//==================================================================================================
 
 // On click event to add new button to favorite gif buttons
 $(".submit-gif").on("click", function(event) {
@@ -26,17 +35,18 @@ $(".submit-gif").on("click", function(event) {
   
   // Validate that the user enters a non empty string.
   if (searchTerm == "") {
-    alert("Please enter something!");
+    alert("Please enter a valid search term.");
   } else {
-    
     // push the input from the input field into the gif array
     gifArray.push(searchTerm);
     console.log(gifArray);
+    // call function to generate button
+    createButton();
   }
-  
-
-
 });
+
+// call function to generate inital buttons
+createButton();
 
 
 // Display Gifs
@@ -45,14 +55,14 @@ $(".submit-gif").on("click", function(event) {
 // On click fuction that displays the gifs depending on the button that is clicked
 $(".gif-display").on("click", function(event) {
   
-  event.preventDefault();
+  var searchTerm = $(this).attr("data-button");
+  console.log(searchTerm);
   
   var apiKey = "mJ6YiwAKwAYk4h496ybr2uLF4xCC5tSK";
    
-  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchTerm +  "&api_key=" + apiKey;
-    
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + "dog" +  "&api_key=" + apiKey;
 
-   
+
   // Query GIPHY API
   $.ajax({
     url: queryURL,
@@ -72,6 +82,9 @@ $(".gif-display").on("click", function(event) {
   
   
 });
+
+
+
 
 
 
